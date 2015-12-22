d6e46c8a225629f376cf365582073d1237fe8035
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2015-12-21 17:16:13 -0800

    add type script environment variable

--- config/laradit.php
+++ config/laradit.php
@@ -6,5 +6,6 @@ return [
   'oauth_redirect_uri' => env('LARADIT_OAUTH_CLIENT_REDIRECT_URL'),
   'user_agent' => env('LARADIT_USER_AGENT', 'laradit'),
   'reddit_username' => env('LARADIT_REDDIT_USERNAME'),
-  'reddit_password' => ENV('LARADIT_REDDIT_PASSWORD')
+  'reddit_password' => ENV('LARADIT_REDDIT_PASSWORD'),
+  'type_script' => ENV('LARADIT_TYPE_SCRIPT')
 ];
