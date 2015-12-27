523104ecea94a6843b156034f0afdb95e39685b8
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2015-12-27 13:31:10 -0800

    rename APIRequestBuilder to APIRequestHelper + add getJsonResponse to extract json body from guzzle response

--- src/JD/Laradit/Helpers/APIRequestHelper.php
+++ src/JD/Laradit/Helpers/APIRequestHelper.php
@@ -1,11 +1,12 @@
 <?php namespace JD\Laradit\Helpers;
+use GuzzleHttp;
 
-class APIRequestBuilder
+class APIRequestHelper
 {
     public static function getRequestAttributes($url, $token)
     {
         return [
-            'url' => 'https://oauth.reddit.com/api/'.$url,
+            'url' => 'https://oauth.reddit.com/api/v1/'.$url,
             'headers' => [
                 'headers' => [
                     'User-Agent' => 'my user agent',
@@ -13,5 +14,14 @@ class APIRequestBuilder
                 ]
             ]
         ];
+    }
+
+    public static function getJsonResponse(GuzzleHttp\Psr7\Response $res)
+    {
+        if ($res->getStatusCode() == 200) {
+            return json_decode($res->getBody()->getContents(), true);
+        }
+
+        return [];
     }
 }
