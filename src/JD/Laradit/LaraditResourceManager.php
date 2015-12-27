0a75125809929f467739fbe80d62ea87f14ed814
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2015-12-27 13:31:45 -0800

    set token + getter for user resource api wrapper

--- src/JD/Laradit/LaraditResourceManager.php
+++ src/JD/Laradit/LaraditResourceManager.php
@@ -1,10 +1,19 @@
 <?php namespace JD\Laradit;
 
 
+use JD\Laradit\Resources\UserResource;
+
 class LaraditResourceManager
 {
-    public function __construct()
-    {
+    protected $authToken;
 
+    public function setAuthToken($authToken)
+    {
+        $this->authToken = $authToken;
+    }
+
+    public function getUserResource()
+    {
+        return new UserResource($this->authToken);
     }
 }
