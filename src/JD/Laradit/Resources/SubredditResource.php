be2a0c5d42707cd18d8357b56d4dae90e55b9c27
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2015-12-27 16:25:37 -0800

    Add subreddit resource

--- src/JD/Laradit/Resources/SubredditResource.php
+++ src/JD/Laradit/Resources/SubredditResource.php
@@ -1,7 +1,41 @@
 <?php namespace JD\Laradit\Resources;
 
 
+use JD\Laradit\Helpers\APIRequestHelper;
+
 class SubredditResource
 {
+    protected $authToken;
+
+    public function __construct($authToken)
+    {
+        $this->authToken = $authToken;
+    }
+
+    public function getAboutSubreddit($subreddit_name)
+    {
+        $res = APIRequestHelper::createGetRequest('r/'.$subreddit_name.'/about', $this->authToken);
+        return APIRequestHelper::getJsonResponse($res);
+    }
+
+    public function getRecommendedSubreddits($subreddit_names)
+    {
+        $subreddit_names = join(',', $subreddit_names);
+        $res = APIRequestHelper::createGetRequest('api/recommend/sr/'.$subreddit_names, $this->authToken);
+        return APIRequestHelper::getJsonResponse($res);
+    }
+
+    public function searchSubredditsByName($substring)
+    {
+        $res = APIRequestHelper::createPostRequest('api/search_reddit_names/', $this->authToken, ['query' => $substring]);
+        return APIRequestHelper::getJsonResponse($res);
+    }
+
+    public function searchSubredditsByTopic($topic)
+    {
+        $res = APIRequestHelper::createGetRequest('api/subreddits_by_topic/', $this->authToken, ['query' => $topic]);
+        return APIRequestHelper::getJsonResponse($res);
+    }
+
 
 }
