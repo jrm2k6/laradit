1f6c52b2c379a3ac136d481170492ea17bd1fad2
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-03-15 16:53:42 -0700

    fix env key name in config file

--- config/laradit.php
+++ config/laradit.php
@@ -3,7 +3,7 @@
 return [
   'client_id' => env('LARADIT_CLIENT_ID'),
   'client_secret' => env('LARADIT_CLIENT_SECRET'),
-  'oauth_redirect_uri' => env('LARADIT_OAUTH_CLIENT_REDIRECT_URL'),
+  'oauth_redirect_uri' => env('LARADIT_OAUTH_REDIRECT_URI'),
   'user_agent' => env('LARADIT_USER_AGENT', 'laradit'),
   'reddit_username' => env('LARADIT_REDDIT_USERNAME'),
   'reddit_password' => ENV('LARADIT_REDDIT_PASSWORD'),
